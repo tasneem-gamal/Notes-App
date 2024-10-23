7ada5956d26b766dfe53918df389f9c212efe597
tasneem-gamal <tasneemgamal044@gmail.com>
2024-10-23 15:11:40 -0700

    create route for edit note view

--- lib/features/home/widgets/note_item.dart
+++ lib/features/home/widgets/note_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:notes/core/helpers/extension.dart';
+import 'package:notes/core/routing/routes.dart';
 import 'package:notes/core/theming/colors.dart';
 import 'package:notes/core/theming/styles.dart';
 
@@ -10,41 +12,46 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 160.w,
-      height: 180.h,
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(12),
-          topRight: Radius.circular(12),
-          bottomLeft: Radius.circular(12),
+    return GestureDetector(
+      onTap: (){
+        context.pushNamed(Routes.editView);
+      },
+      child: Container(
+        width: 160.w,
+        height: 180.h,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(12),
+            topRight: Radius.circular(12),
+            bottomLeft: Radius.circular(12),
+          ),
+          color: ColorManger.kPrimaryColor
         ),
-        color: ColorManger.kPrimaryColor
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Today',
-              style: Styles.textStyle18.copyWith(color: Colors.white),
-            ),
-            SizedBox(height: 8.h,),
-            Text(
-              '8:00 PM',
-              style: Styles.textStyle18.copyWith(color: Colors.white),
-            ),
-            SizedBox(height: 8.h,),
-            Text(
-              'Doctor Appointmet bla bla blka',
-              style: Styles.textStyle16.copyWith(
-                color: Colors.white
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Today',
+                style: Styles.textStyle18.copyWith(color: Colors.white),
               ),
-              overflow: TextOverflow.ellipsis,
-              maxLines: 2,
-            ),
-          ],
+              SizedBox(height: 8.h,),
+              Text(
+                '8:00 PM',
+                style: Styles.textStyle18.copyWith(color: Colors.white),
+              ),
+              SizedBox(height: 8.h,),
+              Text(
+                'Doctor Appointmet bla bla blka',
+                style: Styles.textStyle16.copyWith(
+                  color: Colors.white
+                ),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+            ],
+          ),
         ),
       ),
     );
