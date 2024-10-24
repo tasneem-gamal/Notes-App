8d4dab7ecfa006b8a708d36686a68b3f899435d2
tasneem-gamal <tasneemgamal044@gmail.com>
2024-10-23 20:42:19 -0700

    test

--- lib/features/edit_note/widgets/edit_note_view_body.dart
+++ lib/features/edit_note/widgets/edit_note_view_body.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:notes/core/widgets/constants.dart';
-import 'package:notes/features/edit_note/widgets/custom_app_bar.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:notes/core/widgets/constants.dart';
+// import 'package:notes/features/edit_note/widgets/custom_app_bar.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: Constants.editPadding,
-      child: const Column(
-        children: [
-          CustomAppBar()
-        ],
+    return Expanded(
+      child: Container(
+        color: Colors.red,
+        child: const TextField(
+          maxLines: null,
+          expands: true,
+          decoration: InputDecoration(
+            hintText: 'Type something....',
+            border: InputBorder.none
+          ),
+          keyboardType: TextInputType.multiline,
+        ),
       ),
     );
   }
