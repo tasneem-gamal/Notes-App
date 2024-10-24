d08053d7c4365fbf86f951d7d27be8d6374d989d
tasneem-gamal <tasneemgamal044@gmail.com>
2024-10-23 20:22:33 -0700

    edit view app bar

---
+++ lib/features/edit_note/widgets/edit_note_view_body.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:notes/core/widgets/constants.dart';
+import 'package:notes/features/edit_note/widgets/custom_app_bar.dart';
+
+class EditNoteViewBody extends StatelessWidget {
+  const EditNoteViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: Constants.editPadding,
+      child: const Column(
+        children: [
+          CustomAppBar()
+        ],
+      ),
+    );
+  }
+}
