ce7f35731de14dccb218d5904d44983b30574291
tasneem-gamal <tasneemgamal044@gmail.com>
2024-10-26 13:51:05 -0700

    refactor notes cubit

--- lib/features/home/logic/read_notes_cubit/notes_cubit.dart
+++ lib/features/home/logic/read_notes_cubit/notes_cubit.dart
@@ -11,12 +11,9 @@ class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
 
   fetchAllNotes() async {
-    try {
       var notesBox = Hive.box<NoteModel>(Constants.kNotesBox);
       List<NoteModel> notes = notesBox.values.toList();
       emit(NotesSuccess(notes));
-    } catch (e) {
-      emit(NotesFailure(e.toString()));
     }
   }
-}
+
--- lib/features/home/widgets/note_item_grid_view.dart
+++ lib/features/home/widgets/note_item_grid_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/features/home/logic/read_notes_cubit/notes_cubit.dart';
 import 'package:notes/features/home/widgets/note_item.dart';
 
 class NoteItemGridView extends StatelessWidget {
@@ -8,18 +10,17 @@ class NoteItemGridView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: GridView.builder(
-        shrinkWrap: true,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 12,
-          mainAxisSpacing: 12
-        ),
-        itemCount: 10, 
-        itemBuilder: (context, index) => const NoteItem()
-      ),
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        return Expanded(
+          child: GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, crossAxisSpacing: 12, mainAxisSpacing: 12),
+              itemCount: 10,
+              itemBuilder: (context, index) => const NoteItem()),
+        );
+      },
     );
   }
 }
-
