718677133d12d9fee4fea7d4d903a92d2ff7e5c7
tasneem-gamal <tasneemgamal044@gmail.com>
2024-10-26 10:57:14 -0700

    register note type adaptor

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes/core/routing/app_router.dart';
 import 'package:notes/core/widgets/constants.dart';
+import 'package:notes/features/home/data/note_model.dart';
 import 'package:notes/notes.dart';
 
 void main() async {
@@ -9,6 +10,8 @@ void main() async {
   await Hive.initFlutter();
 
   await Hive.openBox(Constants.kNotesBox);
+
+  Hive.registerAdapter(NoteModelAdapter());
 
   runApp(Notes(appRouter: AppRouter()));
 }
